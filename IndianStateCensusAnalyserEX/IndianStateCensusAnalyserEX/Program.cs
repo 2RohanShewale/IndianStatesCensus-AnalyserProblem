2f9f625acd61f394da7e915476640b8745feacf2
Rohan Shewale <shewalerohan50@gmail.com>
2023-03-20 18:48:10 +0530

    [Rohan] load the Indian States Census Information from a csv file

---
+++ IndianStateCensusAnalyserEX/IndianStateCensusAnalyserEX/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnalyserEx
+{
+    internal class Program
+    {
+        public static string filePath= @"C:\Users\shewa\RFP-256\IndianStateCensusAnalyserEx\IndianStateCensusAnalyserEx\Files\StateCensusData.csv";
+        static void Main(string[] args)
+        {
+
+            CsvStateCensus csv = new CsvStateCensus();
+            csv.ReadStateCensusData(filePath);
+            Console.ReadKey();
+        }
+    }
+}
