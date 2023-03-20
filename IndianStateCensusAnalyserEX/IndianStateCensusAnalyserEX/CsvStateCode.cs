8a2f62cd6b9c42bb394fc7370c3d0fb255dffba0
Rohan Shewale <shewalerohan50@gmail.com>
2023-03-21 02:31:12 +0530

    [Rohan]Read State Code From csv

---
+++ IndianStateCensusAnalyserEX/IndianStateCensusAnalyserEX/CsvStateCode.cs
@@ -0,0 +1,29 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnalyserEX
+{
+    internal class CsvStateCode
+    {
+        public void ReadStateCode(string filePath)
+        {
+            using (var streamReader = new StreamReader(filePath))
+            {
+                using (var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture))
+                {
+                    var records = csvReader.GetRecords<StateCodeModel>().ToList();
+                    foreach (var record in records)
+                    {
+                        Console.WriteLine($"{record.SrNo} {record.StateName} {record.TIN} {record.StateCode}");
+                    }
+                }
+            }
+        }
+    }
+}
--- IndianStateCensusAnalyserEX/IndianStateCensusAnalyserEX/Program.cs
+++ IndianStateCensusAnalyserEX/IndianStateCensusAnalyserEX/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IndianStateCensusAnalyserEX;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,12 +9,16 @@ namespace IndianStateCensusAnalyserEx
 {
     internal class Program
     {
-        public static string filePath= @"C:\Users\shewa\RFP-256\IndianStateCensusAnalyserEx\IndianStateCensusAnalyserEx\Files\StateCensusData.csv";
+        public static string stateCensusDataPath= @"C:\Users\shewa\RFP-256\IndianStateCensusAnalyserEx\IndianStateCensusAnalyserEx\Files\StateCensusData.csv";
+        public static string stateCodePath = @"C:\Users\shewa\RFP-256\IndianStatesCensus-AnalyserProblem\IndianStateCensusAnalyserEX\IndianStateCensusAnalyserEX\Files\StateCode.csv";
         static void Main(string[] args)
         {
 
-            CsvStateCensus csv = new CsvStateCensus();
-            csv.ReadStateCensusData(filePath);
+            //CsvStateCensus csv = new CsvStateCensus();
+            //csv.ReadStateCensusData(filePath);
+            CsvStateCode csvStateCode = new CsvStateCode();
+            csvStateCode.ReadStateCode(stateCodePath);
+
             Console.ReadKey();
         }
     }
